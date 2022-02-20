78fd0f4ed4e53c7fc9d4d5f26911addaa12a2e40
Cheshire Cat <Sasha-Km@yandex.ru>
2022-02-20 13:31:34 +0300

    correct code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгортма. 
+
+//При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+void PrintStringArray(string[] stringArray)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.Write($"\"{stringArray[i]}\"");
+        if (i != stringArray.Length -1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+int SearchStringsWithSizeLessOrEqual3(string[] stringArray)
+{
+    int numberOfStrings = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+           numberOfStrings++; 
+        }
+    }
+    return numberOfStrings;
+}
+
+string[] CreateArrayWithSizeLessOrEqual3(string[] stringArray, int sizeNewArray)
+{
+    string[] newStringArray = new string[sizeNewArray];
+    int j = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+           newStringArray[j] = stringArray[i]; 
+           j++;
+        }
+    }
+    return newStringArray;
+}
+
+string[] stringArray = { "hello", "word", "2", ":-)"};
+PrintStringArray(stringArray);
+int sizeNewArray = SearchStringsWithSizeLessOrEqual3(stringArray); // переименовать
+string[] newStringArray = CreateArrayWithSizeLessOrEqual3(stringArray, sizeNewArray);
+Console.Write(" => ");
+PrintStringArray(newStringArray);
+Console.WriteLine();
